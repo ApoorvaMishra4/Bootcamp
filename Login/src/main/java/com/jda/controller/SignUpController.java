f39efeaad4193ed95901a9b4b82148389f98f0a5
Apoorva Mishra <apoorva.mishra@jda.com>
2018-08-09 07:37:40 +0000

    SignUpController fix

--- Login/src/main/java/com/jda/controller/SignUpController.java
+++ Login/src/main/java/com/jda/controller/SignUpController.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,18 +14,21 @@ import javax.servlet.http.HttpServletResponse;
 
 public class SignUpController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private static String jdbcUrl = "jdbc:mysql://" + System.getenv("DBHOST") + ":3306/" + System.getenv("DBNAME") + "?user="+ System.getenv("DBUSER") +"&password="+System.getenv("DBPASSWORD");
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mysql://10.0.0.160/db100033", System.getenv("DBUSER"), System.getenv("DBPASSWORD"));
+			Connection connection = DriverManager.getConnection(jdbcUrl);
 			PreparedStatement pst = connection.prepareStatement("insert into data (name, username, password, phoneNumber) values(?, ?, ?, ?)");
-			System.out.println("ha");
 			pst.setString(1, request.getParameter("name"));
 			pst.setString(2, request.getParameter("username"));
 			pst.setString(3, request.getParameter("password"));
 			pst.setString(4, request.getParameter("phoneNumber"));
 			pst.executeUpdate();
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher(response.encodeRedirectUrl("Login.jsp"));
+			requestDispatcher.forward(request, response);
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
